2c9b810745834f4f4d578f9b84d8d89b4e239009
JON J <gr8fuldwg@gmail.com>
2018-11-02 20:52:54 -0400

    launched working local host with 'it works' and added classes to html content

--- src/App.js
+++ src/App.js
@@ -5,8 +5,17 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div>
-        It works
+      <div className="App-container">
+        <header className="App-header">
+          <h1 className="">Box-Color</h1>
+        </header>
+
+        <main className="App-main">
+          <div></div>
+        </main>
+        <footer className="App-footer">
+          <p>Made by Jon!!!</p>
+        </footer>
       </div>
     );
   }
