4664996932ac6d5a6631355300ae4a2e1b3115ec
JON J <gr8fuldwg@gmail.com>
2018-11-05 18:45:28 -0500

    Updates

---
+++ precache-manifest.093c14e06a7ee634fe21c7f25e3724c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "997c74cf7251adf80e3a",
+    "url": "/box-color/static/js/runtime~main.997c74cf.js"
+  },
+  {
+    "revision": "aa76dcb546edd73605ad",
+    "url": "/box-color/static/js/main.aa76dcb5.chunk.js"
+  },
+  {
+    "revision": "8bdcd94eba6be17cc0fe",
+    "url": "/box-color/static/js/1.8bdcd94e.chunk.js"
+  },
+  {
+    "revision": "aa76dcb546edd73605ad",
+    "url": "/box-color/static/css/main.9c3ecd02.chunk.css"
+  },
+  {
+    "revision": "65e5cb7587259f158a169a37395597c8",
+    "url": "/box-color/index.html"
+  }
+];
